164302e13416cb12484f3d69dad4f68d81c9646f
Sheeba <sheeba.muthukrishna@gmail.com>
2017-06-18 10:23:57 -0500

    Committing Automation
    Signed-off-by: Sheeba <sheeba.muthukrishna@gmail.com>

---
+++ E2ESeleniumJenkins/src/testcases/TC_003.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+	
+	@Test
+	public void testcase1(){
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+		driver.findElementByName("q").sendKeys("Selenium");
+		driver.quit();
+		//driver.quit();
+	}
+
+}
